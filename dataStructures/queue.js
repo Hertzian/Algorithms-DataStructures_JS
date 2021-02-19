f3a04fdeddb3269cba5784a9723ed5cdd50368a8
Lalo Aguilar <tallitosan@gmail.com>
2021-02-19 12:30:03 -0600

    Queue class

---
+++ dataStructures/queue.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(val) {
+    this.value = val
+    this.next = null
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null
+    this.last = null
+    this.size = 0
+  }
+}
