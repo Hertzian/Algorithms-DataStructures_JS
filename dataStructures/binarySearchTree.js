a2892b70ef04e3342ccd62e1b63d61580bb57888
Lalo Aguilar <tallitosan@gmail.com>
2021-02-21 13:59:28 -0600

    insert & insert2 to binarySearchTree class

--- dataStructures/binarySearchTree.js
+++ dataStructures/binarySearchTree.js
@@ -1,4 +1,4 @@
-class Nodee {
+class Node {
   constructor(value) {
     this.value = value
     this.left = null
@@ -7,7 +7,7 @@ class Nodee {
 }
 
 class BinarySearchTree {
-  constructor () {
+  constructor() {
     this.root = null
   }
 
@@ -25,13 +25,97 @@ class BinarySearchTree {
    *  -If there is, move to that node and repeat these steps
    *  -If there is not, add that node as the left property
    */
-  insert() {
-    
+  insert(value) {
+    let newNode = new Node(value)
+
+    if (this.root === null) {
+      this.root = newNode
+      return this
+    }
+
+    let current = this.root
+
+    while (true) {
+      if (value === current.value) {
+        return undefined
+      }
+
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode
+          return this
+        }
+
+        current = current.left
+      } else if (value > current.value) {
+        if (current.right === null) {
+          current.right = newNode
+          return this
+        }
+
+        current = current.right
+      }
+    }
   }
+
+  insert2(value) {
+    let newNode = new Node(value)
+
+    if (this.root === null) {
+      this.root = newNode
+      return this
+    }
+
+    let current = this.root
+
+    while (true) {
+      if (value === current.value) {
+        return undefined
+      }
+
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode
+          return this
+        }
+
+        current = current.left
+      }
+
+      if (value > current.value) {
+        if (current.right === null) {
+          current.right = newNode
+          return this
+        }
+
+        current = current.right
+      }
+    }
+  }
+
+  /**
+   * Starting at the root
+   * -Check if there is a root, if not, we're done searching!
+   * -If there is a root, check if the value of the new node is the value we are looking for
+   *  -If we found it, we're done!
+   * -If not, check to see if the value is greater than or less than the value of the root
+   * -If it is greater
+   *  -Check to see if there is a node to the right
+   *    -If there is, move to that node and repeat these steps
+   *    -If there is not, we're done searching!
+   * -If it is less
+   *  -Check to see if there is a node to the left
+   *    -If there is, move to that node and repeat these steps
+   *    -If there is not, we're done searching!
+   */
+  search(valu) {}
 }
 
-
 let tree = new BinarySearchTree()
-tree.root.right = new Node(15)
-tree.root.left = new Node(7)
-tree.root.left.right = new Node(9)
+tree.insert(10)
+tree.insert(5)
+tree.insert(13)
+tree.insert(11)
+tree.insert(2)
+tree.insert(16)
+tree.insert(7)
