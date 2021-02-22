968e59dc4a9b9f227255591516aad5185831f561
Lalo Aguilar <tallitosan@gmail.com>
2021-02-22 15:12:41 -0600

    breadthFirstSearch method to binarySearchTree class

--- dataStructures/binarySearchTree.js
+++ dataStructures/binarySearchTree.js
@@ -153,6 +153,38 @@ class BinarySearchTree {
 
     return false
   }
+
+  /**
+   * Create a queue (this can be an array) and a variable to store the values of nodes visited
+   * Place the rrot node in the queue
+   * Loop as long as there is anything in the queue
+   *  -Dequeue a node from the queue and push the value of the node into the variable that stores the nodes
+   *  -If there is a left property on the node dequeued - add it to the queue
+   *  -If there is a right property on the node dequeued - add it to the queue
+   * Return the variable that stores the values
+   */
+  breadthFirstSearch(){
+    let data = [],
+      queue = [],
+      node = this.root
+
+    queue.push(node)
+
+    while(queue.length) {
+      node = queue.shift()
+      data.push(node.value)
+
+      if(node.left) { 
+        queue.push(node.left)
+      }
+
+      if(node.right) {
+        queue.push(node.right)
+      }
+
+      return data
+    }
+  }
 }
 
 let tree = new BinarySearchTree()
