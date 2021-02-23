66bacd3c9e106a462fc2c534eb6cde0efb0fd946
Lalo Aguilar <tallitosan@gmail.com>
2021-02-23 00:16:26 -0600

    Deep First Search - inOrder method in binarySearchTree class

--- dataStructures/binarySearchTree.js
+++ dataStructures/binarySearchTree.js
@@ -232,18 +232,51 @@ class BinarySearchTree {
    */
   postOrder() {
     let data = []
-    let current = this.root
+    const current = this.root
+
+    function traverse(node) {
+      if (node.left) {
+        traverse(node.left)
+      }
+
+      if (node.right) {
+        traverse(node.right)
+      }
+
+      data.push(node.value)
+    }
+
+    traverse(current)
+
+    return data
+  }
+
+  /**
+   * DFS - Deep First Search
+   * InOrder
+   * Create a variable to store the values of nodes visited
+   * Store the root of the BST in a variable called current
+   * Write a helper function which accepts a node
+   *  -If the node has a left property, call the helper function with the left property on the node
+   *  -Push the value of the node to the variable that stores the values
+   *  -If the node has a right property, call the helper funciton with the right property on the node
+   *  -Invoke the helper funciton with the current variable
+   * Return the array of values
+   */
+  inOrder() {
+    let data = []
+    const current = this.root
 
     function traverse(node) {
       if(node.left) {
         traverse(node.left)
       }
 
+      data.push(node.value)
+
       if(node.right) {
         traverse(node.right)
       }
-
-      data.push(node.value)
     }
 
     traverse(current)
