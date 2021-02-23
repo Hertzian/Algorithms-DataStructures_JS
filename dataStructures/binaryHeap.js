d8bf2a973132bde01562a73b886ec5b09bb9025c
Lalo Aguilar <tallitosan@gmail.com>
2021-02-23 14:28:16 -0600

    added extractMax & sinkDown methods to MaxBinaryHeap class

--- dataStructures/binaryHeap.js
+++ dataStructures/binaryHeap.js
@@ -44,4 +44,71 @@ class MaxBinaryHeap {
       idx = parentIdx
     }
   }
+
+  /**
+   * Swap the first value in the values property with the last one
+   * Pop from the values property, so you can return the value at the end
+   * Have the new root 'sink down' to the correct spot...
+   *  -Your parent index starts at 0 (the root)
+   *  -Find the index of the left child: 2 * index + 1 ( make sure its not out of bounds)
+   *  -Find the index of the right child: 2 * index + 1 ( make sure its not out of bounds)
+   *  -If the left or right child is greater than the element ... swap. If both left and right children are larger, swap with the largest child
+   *  -The child index you swapped to now becomes the new parent index
+   *  -Keep looping and swapping until neither child is larger than the element
+   *  -Return the old root!
+   */
+  extractMax() {
+    // edge case come back to this
+    const max = this.values[0]
+    const end = this.values.pop()
+
+    if(this.values.length > 0) {
+      this.values[0] = end
+      // tricle down
+      this.sinkDonw()
+    }
+
+    return max
+  }
+
+  sinkDown() {
+    let idx = 0
+    const length = this.values.length
+    const element = this.values[0]
+
+    while (true) {
+      let lefChildIdx = 2 * idx + 1
+      let rightChildIdx = 2 * idx + 2
+      let leftChild, rightChild
+      let swap = null
+
+      if (lefChildIdx < length) {
+        leftChild = this.values[lefChildIdx]
+
+        if (leftChild > element) {
+          swap = lefChildIdx
+        }
+      }
+
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx]
+
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIdx
+        }
+      }
+
+      if (swap === null) {
+        break
+      }
+
+      this.values[idx] = this.values[swap]
+      this.values[swap] = element
+      idx = swap
+
+    }
+  }
 }
