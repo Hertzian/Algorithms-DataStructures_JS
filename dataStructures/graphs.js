ca8b52b3faadcf85aeda1583d9acf36ca4530515
Lalo Aguilar <tallitosan@gmail.com>
2021-02-26 00:45:33 -0600

    removeEdge method to Graph class

--- dataStructures/graphs.js
+++ dataStructures/graphs.js
@@ -19,7 +19,26 @@ class Graph {
    * The function should find in the adjacency list the key of vertex2 and push vertex1 to the array
    * Don't worry about handling errors/invalid vertices
    */
-  addEdge(vertex1, vertex2) {
-    
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2)
+    this.adjacencyList[v2].push(v1)
   }
+
+  /**
+   * This function should accept two vertices, we'll call them vertex1 and vertex2
+   * The function should reassing the key of vertex1 to be an array that does not contain vertex2
+   * The function should reassign the key of vertex2 to be an array that does not contain vertex1
+   * Don't worry about handling errors/invalid vertices
+   */
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter((v) => {
+      v !== vertex2
+    })
+
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter((v) => {
+      v !== vertex1
+    })
+  }
+
+  
 }
