bebe8aaae4c42160967cce78ae9c23a3500daa22
Hertzian <tallitosan@gmail.com>
2022-02-15 14:54:57 -0600

    modification to some algs

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -4,20 +4,17 @@ function takeShower() {
 
 function eatBreakfast() {
   let meal = cookFood()
-
   return console.log(`Eating ${meal}`)
 }
 
 function cookFood() {
   let items = ['Oatmeal', 'Eggs', 'Protein shake']
-
   return items[Math.floor(Math.random() * items.length)]
 }
 
 function wakeUp() {
   takeShower()
   eatBreakfast()
-
   return console.log('Ok, ready to work!')
 }
 
@@ -29,11 +26,8 @@ function countDown(num) {
     console.log('All done recursive')
     return
   }
-
   console.log(num)
-
   num--
-
   countDown(num)
 }
 
@@ -44,20 +38,14 @@ function countDownRecursive(num) {
   for (let i = num; i > 0; i--) {
     console.log(i)
   }
-
   console.log('All done iterative')
 }
-
 // countDownRecursive(5);
 
 function sumRange(num) {
-  if (num === 1) {
-    return 1
-  }
-
+  if (num === 1) return 1
   return num + sumRange(num - 1)
 }
-
 // sumRange(10);
 
 function factorialIter(num) {
@@ -69,49 +57,27 @@ function factorialIter(num) {
 }
 
 function factorial(num) {
-  if (num === 1) {
-    return 1
-  }
-
+  if (num === 1) return 1
   return num * factorial(num - 1)
 }
-
 // recursion helper recursion
 
 function collectOdds(nums) {
   let result = []
-
   function helper(helperInut) {
-    if (helperInut.length === 0) {
-      return
-    }
-
-    if (helperInut[0] % 2 !== 0) {
-      result.push(helperInut[0])
-    }
-
+    if (helperInut.length === 0) return
+    if (helperInut[0] % 2 !== 0) result.push(helperInut[0])
     helper(helperInut.slice(1))
   }
-
   helper(arr)
-
   return result
 }
-
 // without helper...
 
 function collectOddValues(arr) {
   let newArr = []
-
-  if (arr.legth == 0) {
-    return newArr
-  }
-
-  if (arr[0] % 2 !== 0) {
-    newArr.push(arr[0])
-  }
-
+  if (arr.legth == 0) return newArr
+  if (arr[0] % 2 !== 0) newArr.push(arr[0])
   newArr = newArr.concat(collectOddValues(arr.slice(1)))
-
   return newArr
 }
